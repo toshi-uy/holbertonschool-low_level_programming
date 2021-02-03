79324fa27c5dbb52ca285c21ca6d106a003d9f11
root <s_borgia@hotmail.com>
2021-02-03 17:26:13 +0000

    betty!

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,13 +1,11 @@
 #include <stdio.h>
-
 /**
- * C program that prints exactly "Programming is like building a multilingual puzzle, followed by a new line.
+ * main - Entry point
  *
  * Return: Always 0 (Success)
  */
-int main()
+int main(void)
 {
-  puts ("\"Programming is like building a multilingual puzzle");
-  
-  return (0);
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
 }
