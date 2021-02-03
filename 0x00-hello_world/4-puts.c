215ce261ed4f8f9c47b885cbcd71b6208f46d833
root <s_borgia@hotmail.com>
2021-02-03 17:18:04 +0000

    4

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * C program that prints exactly "Programming is like building a multilingual puzzle, followed by a new line.
+ *
+ * Return: Always 0 (Success)
+ */
+int main()
+{
+  puts ("\"Programming is like building a multilingual puzzle");
+  
+  return (0);
+}
