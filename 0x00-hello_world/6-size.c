9f1a3c607948e342109cab4d42e44a4572694f91
root <s_borgia@hotmail.com>
2021-02-03 18:10:20 +0000

    6

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+/**
+ *main - Entry point
+ *
+ * Description: This is a longer description.
+ *
+ * Return: 0
+*/
+int main(void)
+{
+printf("Size of char: %d byte(s)\n", sizeof(charType));
+printf("Size of int: %d bytes(s)\n", sizeof(intType));
+printf("Size of long int: %d bytes(s)\n", sizeof(long int));
+printf("Size of long long: %d bytes(s)\n", sizeof(long long int));
+printf("Size of float: %d bytes(s)\n", sizeof(floatType));
+return (0);
+}
