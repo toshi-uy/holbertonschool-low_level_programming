3befff562a18ad43ad34db2b54673158f73b8d8b
root <s_borgia@hotmail.com>
2021-02-03 17:45:18 +0000

    5

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,11 +1,12 @@
 #include <stdio.h>
-/**
- * main - Entry point
+ /**
+ * Hello, printf
  *
+ * Description: show a message sdajdas
  * Return: Always 0 (Success)
  */
 int main(void)
 {
-  printf("with proper grammar, but the outcome is a piece of art,")
+printf("with proper grammar, but the outcome is a piece of art,\n");
 return (0);
 }
