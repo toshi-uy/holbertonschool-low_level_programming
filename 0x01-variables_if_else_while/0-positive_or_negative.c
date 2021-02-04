726c8772838068d9a8380d9374e72b1404f400f4
root <s_borgia@hotmail.com>
2021-02-04 12:47:48 +0000

    0

---
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,21 @@
+#include <stdlib.h>
+/**
+ *main - Entry point
+ *
+ *Description: Positive anything is better than negative nothing
+ *
+ *Return: Always 0 (Success)
+ */
+int main(void)
+{
+int n;
+srand(time(0));
+n = rand() - RAND_MAX / 2;
+if (n > 0)
+printf("%d is positive\n", n);
+else if (n < 0)
+printf("%d is a negative\n", n);
+else if (n == 0)
+printf("%d is zero\n", n);
+return (0);
+}
