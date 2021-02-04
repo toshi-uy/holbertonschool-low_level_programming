c3c45b31881e2de14fd2573a8cc78202b9f5a913
root <s_borgia@hotmail.com>
2021-02-04 01:08:53 +0000

    101

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,14 @@
+#include <unistd.h>
+/**
+ *main - Entry point
+ *
+ * Description: This is a longer description.
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+write(1, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 58);
+return (1);
+}
