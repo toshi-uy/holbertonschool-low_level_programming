002c89a86811fe51d70f0135bde5772cd5127f4e
root <s_borgia@hotmail.com>
2021-02-04 18:47:28 +0000

    10

---
+++ 0x01-variables_if_else_while/10-print_comb2.c
@@ -0,0 +1,28 @@
+include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <ctype.h>
+/**
+ *main - Entry point
+ *
+ *Description: Positive anything is better than negative nothing
+ *
+ *Return: Always 0 (Success)
+ */
+int main(void)
+{
+        int x;
+
+        for (x = '00' ; x <= '99' ; x++)
+        {
+                putchar(x);
+
+                if (x != '9')
+                {
+			putchar(',');
+			putchar(' ');
+                }
+        }
+        putchar('\n');
+        return (0);
+}
