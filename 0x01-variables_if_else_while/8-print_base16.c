011306d1d60e7f20e73ffabf3a88bb55c60cd54f
root <s_borgia@hotmail.com>
2021-02-04 18:21:59 +0000

    8

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,24 @@
+#include <stdlib.h>
+#include <stdio.h>
+/**
+ *main - Entry point
+ *
+ *Description: Positive anything is better than negative nothing
+ *
+ *Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	char x, y;
+	for (x = '0' ; x <= '9' ; x++)
+	{
+		putchar(x);
+	}
+	for (y = 'A' ; y <= 'F' ; y++)
+	{
+		putchar(y);
+	}                                                                                                                                 }
+	putchar('\n');
+	return (0);
+}
