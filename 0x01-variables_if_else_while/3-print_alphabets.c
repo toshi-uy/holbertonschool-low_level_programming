2172b85a47f7016b94dc4f93881612dbc9486c19
root <s_borgia@hotmail.com>
2021-02-04 17:23:07 +0000

    3

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -7,19 +7,20 @@
  *
  *Return: Always 0 (Success)
  */
-int main (void)
+
+int main(void)
 {
 	char az;
 	char AZ;
 
-	for(az= 'a' ; az <= 'z' ; az++)
+	for (az = 'a' ; az <= 'z' ; az++)
 	{
 		putchar(az);
 	}
-	for(AZ= 'A' ; AZ <= 'Z' ; AZ++)
+	for (AZ = 'A' ; AZ <= 'Z' ; AZ++)
 	{
 		putchar(AZ);
 	}
 	putchar('\n');
-       	return(0);
+	return (0);
 }
