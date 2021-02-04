e39ca2d8c9c247251e07dc58d19be98301bd425c
root <s_borgia@hotmail.com>
2021-02-04 18:08:02 +0000

    7

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,21 @@
+#include <stdlib.h>
+#include <stdio.h>
+/**
+ *main - Entry point
+ *
+ *Description: Positive anything is better than negative nothing
+ *
+ *Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	char za;
+
+	for (za = 'z' ; za >= 'a' ; za++)
+	{
+		putchar(za);
+	}
+	putchar('\n');
+	return (0);
+}
