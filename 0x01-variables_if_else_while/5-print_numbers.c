59c6da3f2816544bfaac4b61c2e0889f8a834253
root <s_borgia@hotmail.com>
2021-02-04 17:47:39 +0000

    5

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,21 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <ctype.h>
+/**
+ *main - Entry point
+ *
+ *Description: Positive anything is better than negative nothing
+ *
+ *Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char x;
+
+	for (x = '0' ; x <= '9' ; x++) {
+		putchar(x);
+	}
+	putchar('\n');
+	return (0);
+}
