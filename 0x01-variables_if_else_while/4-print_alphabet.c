d0fd7468617a901905f4175f6446756752711de7
root <s_borgia@hotmail.com>
2021-02-04 17:31:28 +0000

    4

---
+++ 0x01-variables_if_else_while/4-print_alphabet.c
@@ -0,0 +1,22 @@
+#include <stdlib.h>
+#include <stdio.h>
+/**
+ *main - Entry point
+ *
+ *Description: Positive anything is better than negative nothing
+ *
+ *Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	char az;
+
+	for (az = 'a' ; az <= 'z' ; az++)
+	{
+		if (az != q && az != e)
+			putchar(az);
+	}
+	putchar('\n');
+	return (0);
+}
