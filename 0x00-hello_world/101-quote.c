9a495b04aae61f8a9f19deab6a131b22c2425b46
root <s_borgia@hotmail.com>
2021-02-04 01:14:07 +0000

    fixed spaces

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,14 +1,14 @@
 #include <unistd.h>
 /**
- *main - Entry point
+ *main - quote.c
  *
- * Description: This is a longer description.
+ * Description: printing without printf
  *
  * Return: 0
  */
 
 int main(void)
 {
-write(1, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 58);
+write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 return (1);
 }
