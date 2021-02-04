6e741d872cf03d97e6ab69092d73c7f715c25f08
root <s_borgia@hotmail.com>
2021-02-04 18:45:52 +0000

    9

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -11,14 +11,17 @@
  */
 int main(void)
 {
-	char x, y, z;
+	int x;
 
 	for (x = '0' ; x <= '9' ; x++)
 	{
 		putchar(x);
-		if (x != 9)
-		putchar(' ');
+
+		if (x != '9')
+		{
 		putchar(',');
+		putchar(' ');
+		}
 	}
 	putchar('\n');
 	return (0);
