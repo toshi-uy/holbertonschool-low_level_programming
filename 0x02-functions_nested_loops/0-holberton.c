328afb7e399ad4aea5dd369b01febef486f6ffa7
root <s_borgia@hotmail.com>
2021-02-08 16:40:38 +0000

    0

---
+++ 0x02-functions_nested_loops/0-holberton.c
@@ -0,0 +1,23 @@
+#include "holberton.h"
+
+/**
+ *main - Entry point
+ *
+ *Description: Positive anything is better than negative nothing
+ *
+ *Return: Always 0 (Success)
+ */
+int main(void)
+{
+	_putchar('H');
+	_putchar('o');
+	_putchar('l');
+	_putchar('b');
+	_putchar('e');
+	_putchar('r');
+	_putchar('t');
+	_putchar('o');
+	_putchar('n');
+	_putchar('\n');
+	return (0);
+}
