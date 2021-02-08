87ffa5b76e70296b4f8544c4b59a800faf576327
root <s_borgia@hotmail.com>
2021-02-08 21:04:07 +0000

    7

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,26 @@
+#include "holberton.h"
+
+/**
+ * print_last_digit - checks for alpha character
+ * @x: parameter c checks if character is lowercase
+ *
+ * Description: checks if parameter (int c) is a lowercase character
+ *
+ * Return: Always 0.
+ */
+
+int print_last_digit(int x)
+{
+	x = x % 10;
+
+	if (x < 0)
+	{
+		_putchar(-x);
+		return (-x);
+	}
+	else
+	{
+		_putchar(x);
+		return (x);
+	}
+}
