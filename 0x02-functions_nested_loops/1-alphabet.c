a04891d07cce2372d2071b0647d0f8b149989aa0
root <s_borgia@hotmail.com>
2021-02-08 16:58:04 +0000

    1

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include "holberton.h"
+#include <stdio.h>
+
+/**
+ * main - check the code for Holberton School students.
+ *
+ * Return: Always 0.
+ */
+int print_alphabet(void)
+{	char az;
+
+    	for (az = 'a' ; az <= 'z' ; z++)
+	{
+	_putchar(az);
+	}
+	_putchar('\n');
+    return (0);
+}
