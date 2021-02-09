c4a16c1210ab00ba635d537e091c63118e62e29a
root <s_borgia@hotmail.com>
2021-02-09 20:53:24 +0000

    10

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -2,7 +2,8 @@
 
 /**
  * add - checks for alpha character
- *
+ * @x: parameter x
+ * @y: parameter y
  * Description: checks if parameter (int c) is a lowercase character
  *
  * Return: Always 0.
