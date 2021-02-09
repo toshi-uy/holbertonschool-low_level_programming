e5ec1fec7c2aedc3cf69223081eb5a6e146beca5
root <s_borgia@hotmail.com>
2021-02-09 20:52:07 +0000

    10

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,14 @@
+#include "holberton.h"
+
+/**
+ * add - checks for alpha character
+ *
+ * Description: checks if parameter (int c) is a lowercase character
+ *
+ * Return: Always 0.
+ */
+
+int add(int x, int y)
+{
+	return (x + y);
+}
