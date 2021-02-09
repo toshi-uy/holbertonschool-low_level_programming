239dddbf95b8f9bb01ddf2beb8efea442dc1a280
root <s_borgia@hotmail.com>
2021-02-09 23:01:12 +0000

    1

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -10,12 +10,15 @@ int main(void)
 	int i;
 
 	printf("Infinite loop incoming :(\n");
-	/* never increments so it will always be less than 10 */
+
 	i = 0;
+
 	while (i < 10)
 	{
 		putchar(i);
 	}
+/* i never increments so it will always be less than 10 */
 	printf("Infinite loop avoided! \\o/\n");
+
 	return (0);
 }
