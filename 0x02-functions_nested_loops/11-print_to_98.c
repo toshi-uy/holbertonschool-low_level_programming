442077025e0a66a892cb8d5c9e1f5013a085f820
root <s_borgia@hotmail.com>
2021-02-09 21:28:55 +0000

    11

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -12,8 +12,8 @@
 void print_to_98(int n)
 {
 	for (n = n ; n > 98 ; n--)
-		printf("%d , ", n);
+		printf("%d, ", n);
 	for (n = n ; n < 98 ; n++)
-		printf("%d , ", n);
+		printf("%d, ", n);
 	printf("%d \n", n);
 }
