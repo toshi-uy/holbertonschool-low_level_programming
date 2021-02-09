6bdce1f89f347d78ff33da523d5c9d392e4b5dc6
root <s_borgia@hotmail.com>
2021-02-09 17:56:41 +0000

    9

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -1,32 +1,60 @@
 #include "holberton.h"
 
 /**
+ * num_chico - part of the code
+ * @mul: x * y
+ * @y: parameter y
+ * Description: checks if parameter (int c) is a lowercase character
+ */
+void num_chico(int mul, int y)
+{
+	if (y != 9)
+	{
+		_putchar('0' + mul);
+		_putchar(',');
+		_putchar(' ');
+		_putchar(' ');
+	}
+	else
+	{
+		_putchar('0' + mul);
+		_putchar('\n');
+	}
+}
+/**
  * times_table - prints the 9 times table
  *
- * Description: checks if parameter (int c) is a lowercase character
- *
- * Return: Always 0.
+ *Description: checsadsa
  */
-
 void times_table(void)
 {
-	int x, y;
+	int x, y, mul, u, d;
 
 	for (x = 0 ; x <= 9 ; x++)
 	{
 		for (y = 0 ; y <= 9 ; y++)
 		{
-			if (y != 9)
+			mul = x * y;
+			if (mul <= 9)
 			{
-			_putchar('0' + (x * y));
-			_putchar(',');
-			_putchar(' ');
-			_putchar(' ');
+				num_chico(mul, y);
 			}
 			else
 			{
-				_putchar('0' + (x * y));
-				_putchar('\n');
+				u = mul % 10;
+				d = mul / 10;
+				_putchar('0' + d);
+				_putchar('0' + u);
+				if (y != 9)
+				{
+					_putchar(',');
+					_putchar(' ');
+					_putchar(' ');
+				}
+				else
+				{
+					_putchar('\n');
+				}
 			}
 		}
 	}
