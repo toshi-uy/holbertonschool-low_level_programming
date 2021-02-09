7865e8b738c550f91c493777422c6c9ce80bbc81
root <s_borgia@hotmail.com>
2021-02-09 14:15:44 +0000

    8

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,30 @@
+#include "holberton.h"
+
+/**
+ * jack_bauer - 8-24_hours
+ * @c: parameter c checks if character is lowercase
+ *
+ * Description: prints every minute of the day, starting from 00:00 to 23:59
+ *
+ * Return: Always 0.
+ */
+
+void jack_bauer(void)
+{
+  int hs, min;
+
+  for (hs = 0 ; hs <= 23 ; hs++)
+    {
+      for (min= 0 ; min <= 59 ; min++)
+	{
+	  _putchar((hs /10) + '0');
+	  _putchar((hs %10) + '0');
+	  _putchar(' ');
+	  _putchar(':');
+	  _putchar(' ');
+	  _putchar((min /10) + '0');
+	  _putchar((min %10) + '0');
+	  _putchar('\n');
+	}
+    }
+}
