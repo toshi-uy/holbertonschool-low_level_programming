e0fee6c7b20ddd379e0645e68413f989b19a935a
root <s_borgia@hotmail.com>
2021-02-10 02:24:42 +0000

    101

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -12,10 +12,12 @@ int main(void)
 {
 	int x, y;
 
-	for (y = 1 ; y <= 1023 ; y++)
+	for (y = 1 ; y < 1024 ; y++)
 		if (((y % 3) == 0) || ((y % 5) == 0))
-			x = x + y;
-	printf("%d", x);
-	printf("\n");
+		{
+			x = (x + y);
+		}
+			printf("%d", x);
+			printf("\n");
 	return (x);
 }
