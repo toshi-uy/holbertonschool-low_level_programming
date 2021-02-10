351448d383b34c8c496f027ddf9f61f79e5513d0
root <s_borgia@hotmail.com>
2021-02-10 02:01:52 +0000

    101

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,21 @@
+#include "holberton.h"
+#include <stdio.h>
+
+/**
+ *main - Entry point
+ *
+ *Description: computes and prints the sum of all the multiples of 3 or 5
+ *
+ *Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int x, y;
+
+	for (y = 1023 ; y >= 1 ; y++)
+		if (((y % 3) == 0) || ((y % 5) == 0))
+			x = x + y;
+	printf("%d" , x);
+	printf("\n");
+	return (x);
+}
