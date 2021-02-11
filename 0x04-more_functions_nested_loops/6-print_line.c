6f732ff494ef2335095c3c21c2bd89b13a90e8e8
root <s_borgia@hotmail.com>
2021-02-11 16:16:06 +0000

    6

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,25 @@
+#include "holberton.h"
+
+/**
+ * _islower - checks for lowercase character
+ * @n: parameter n checks if character is lowercase
+ *
+ * Description: checks if parameter (int c) is a lowercase character
+ *
+ * Return: Always 0.
+ */
+
+void print_line(int n)
+{
+	if (n > 0)
+	{
+		while (n != 0)
+		{
+		_putchar('_');
+		n--;
+		}
+		_putchar(10);
+	}
+	else
+		_putchar(10);
+}
