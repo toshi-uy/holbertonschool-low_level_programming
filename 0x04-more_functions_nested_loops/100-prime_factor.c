45e55903b4d346c7b7aaa6c2eff367b650450b84
root <s_borgia@hotmail.com>
2021-02-12 02:16:48 +0000

    100

---
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -0,0 +1,20 @@
+#include "holberton.h"
+
+/**
+ * main - Entry point
+ *
+ * Description - PRIMER NUMBERS
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	long int p, n;
+
+	n = 612852475143;
+	for (p = 2; p < n; p++)
+		if (n % p == 0)
+			n = n / p;
+	printf("%ld\n", );
+	return (0);
+}
