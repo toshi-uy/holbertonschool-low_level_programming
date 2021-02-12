29895b93431efc61bfdbd684a45cde80fb386bcc
root <s_borgia@hotmail.com>
2021-02-12 00:46:49 +0000

    9

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+/**
+ * main - Entry
+ *
+ *Description: prints the alphabert 10 times
+ *
+ *Return (0) Success
+ */
+int main (void)
+{
+	int n;
+
+	for (n = 1; n <= 99; n++)
+		if (n % 3 == 0)
+			if (n % 5 == 0)
+				printf("FizzBuzz ");
+			else
+				printf("Fizz ");
+		else
+			if (n % 5 == 0)
+				printf("Buzz ");
+			else
+				printf("%d ", n);
+	printf("Buzz\n");
+	return (0);
+}
