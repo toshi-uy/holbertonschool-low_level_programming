1d945636f5db1d6aced328be82ca51739c1e9148
root <s_borgia@hotmail.com>
2021-02-12 00:52:14 +0000

    9

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -13,16 +13,22 @@ int main(void)
 	int n;
 
 	for (n = 1; n <= 99; n++)
+	{
 		if (n % 3 == 0)
+		{
 			if (n % 5 == 0)
 				printf("FizzBuzz ");
 			else
 				printf("Fizz ");
+		}
 		else
+		{
 			if (n % 5 == 0)
 				printf("Buzz ");
 			else
 				printf("%d ", n);
+		}
+	}
 	printf("Buzz\n");
 	return (0);
 }
