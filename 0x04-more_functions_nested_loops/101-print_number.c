e8430bff55db7af1f8e961b631e499ffc6577cd8
root <s_borgia@hotmail.com>
2021-02-12 01:43:25 +0000

    101

---
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -0,0 +1,26 @@
+#include "holberton.h"
+
+/**
+ * print_number - checks for uppercase character.
+ * @n: description for n.
+ * Description: checks if parameter (int c) is a uppercase character
+ *
+ * Return: Always 1 if c is uppercase.
+ */
+void print_number(int n)
+{
+	int x;
+
+	if (n < 0)
+        {
+                _putchar('-');
+                x = -n;
+        }
+        else
+		x = n;
+	if (x / 10 != 0)
+	{
+		print_number(x / 10);
+	}
+	_putchar('0' + (x % 10));
+}
