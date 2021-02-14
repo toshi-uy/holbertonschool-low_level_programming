a13af84050686ba6fa7a23007469d93b27511269
root <s_borgia@hotmail.com>
2021-02-14 20:23:06 +0000

    3

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,17 @@
+#include "holberton.h"
+
+/**
+ * _puts - checks for digits
+ * @*str: parameter str
+ * Description: checks if parameter (int c) is digit
+ *
+ */
+void _puts(char *str)
+{
+	while (*str != '\0')
+	{
+		_putchar(*str);
+		str++;
+	}
+	_putchar(10);
+}
