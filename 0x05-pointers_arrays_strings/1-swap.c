d21fd3f53608de9a6c1126e7325c6f211950f544
root <s_borgia@hotmail.com>
2021-02-14 18:38:14 +0000

    1

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,18 @@
+#include "holberton.h"
+/**
+ * swap_int - swaps parameters
+ * @*a: parameter of pointer a
+ * @*b: parameter of pointer b
+ *
+ * Description: swaps two values
+ *
+ */
+
+void swap_int(int *a, int *b)
+{
+	int x;
+
+	x = *b;
+	*b = *a;
+	*a = x;
+}
