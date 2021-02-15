d576bf8c03545ceaaadae3c89d3f6c87e2c568ab
toshi-uy <s_borgia@hotmail.com>
2021-02-15 20:25:36 +0000

    6

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -2,7 +2,7 @@
 
 /**
  * puts2 - checks for digits
- * @*str: parameter str
+ * @str: parameter str
  * Description: checks if parameter (int c) is digit
  *
  */
@@ -10,7 +10,7 @@ void puts2(char *str)
 {
 	while (*str != '\0')
 	{
-		if(*str % 2 == 0)
+		if (*str % 2 == 0)
 		_putchar(*str);
 		str++;
 	}
