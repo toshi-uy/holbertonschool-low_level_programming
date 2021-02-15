39d438b8b15f74213630ead62b0785c5755de8f6
toshi-uy <s_borgia@hotmail.com>
2021-02-15 17:59:11 +0000

    6

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,18 @@
+#include "holberton.h"
+
+/**
+ * puts2 - checks for digits
+ * @*str: parameter str
+ * Description: checks if parameter (int c) is digit
+ *
+ */
+void puts2(char *str)
+{
+	while (*str != '\0')
+	{
+		if(*str % 2 == 0)
+		_putchar(*str);
+		str++;
+	}
+	_putchar(10);
+}
