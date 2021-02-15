d16e672f64771b50e4dd757694a2e0493da2ae48
toshi-uy <s_borgia@hotmail.com>
2021-02-15 14:33:45 +0000

    4

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,41 @@
+#include "holberton.h"
+
+/**
+ * _strlen - counts strings
+ * @s: parameter s
+ *
+ * Description: counts string characters
+ *
+ */
+int _strlen(char *s)
+{
+	char count = 0;
+
+	while (*s != '\0')
+	{
+		count++;
+		s++;
+	}
+	return (count);
+}
+
+/**
+ * print_rev - prints on reverse
+ * @s: parameter s
+ *
+ * Description: prints strings on reverse
+ *
+ */
+void print_rev(char *s)
+{
+	int length, a;
+
+	length = _strlen(s);
+	a = *s;
+
+	for (a = (length -1) ; a >= 0 ; a--)
+	{
+		_putchar(*(s + a));
+	}
+	_putchar(10);
+}
