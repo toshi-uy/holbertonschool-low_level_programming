f14f29b03d6141fc72f0151ee4e53d4229f53c49
toshi-uy <s_borgia@hotmail.com>
2021-02-17 14:00:42 +0000

    100

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -11,12 +11,12 @@
 
 int _atoi(char *s)
 {
-	int sign = 1;
+	int flag = 1;
 	unsigned int num = 0;
 
 	do {
 		if (*s == '-')
-			sign *= -1;
+			flag *= -1;
 
 		else if (*s >= '0' && *s <= '9')
 			num = (num * 10) + (*s - '0');
@@ -25,5 +25,5 @@ int _atoi(char *s)
 			break;
 	} while (*s++);
 
-	return (num * sign);
+	return (num * flag);
 }
