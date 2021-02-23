5eea33b2fb615c6e21cb443f2e35e1817732a1cf
toshi-uy <s_borgia@hotmail.com>
2021-02-23 20:52:11 +0000

    9

---
+++ 0x07-pointers_arrays_strings/9-set_string.c
@@ -0,0 +1,15 @@
+#include "holberton.h"
+
+/**
+ * set_string - Entry point
+ * @s: destination parameter
+ * @to: source parameter
+ *
+ * Return: a pointer to the resulting string dest and 0
+ *
+ */
+
+void set_string(char **s, char *to)
+{
+	*s = to;
+}
