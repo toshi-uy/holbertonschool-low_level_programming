bed4ea939c2a2f85c3358ebd2a6092800a36b102
toshi-uy <s_borgia@hotmail.com>
2021-02-23 15:53:27 +0000

    1

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,22 @@
+#include "holberton.h"
+
+/**
+ * _memcpy - Entry point
+ * @dest: destination parameter
+ * @src: source parameter
+ * @n: n number parameter
+ *
+ * Return: a pointer to the resulting string dest
+ *
+ */
+
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	unsigned int i;
+	char *src_char = (char*)src;
+	char *dest_char = (char*)dest;
+
+	for (i = 0; i < n; i++)
+		dest_char[i] = src_char[i];
+	return(dest);
+}
