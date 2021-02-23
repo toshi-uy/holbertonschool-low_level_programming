94b1a55cec67b9a8ed3f0c8b80c09bd85bf6464d
toshi-uy <s_borgia@hotmail.com>
2021-02-23 18:11:39 +0000

    4

---
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,27 @@
+#include "holberton.h"
+
+/**
+ * _strpbrk - Entry point
+ * @s: destination parameter
+ * @accept: source parameter
+ *
+ * Return: a pointer to the resulting string dest
+ *
+ */
+
+char *_strpbrk(char *s, char *accept)
+{
+	char c, *p;
+
+	for (c = *s ; c != 0 ; s++ , c = *s)
+	{
+		for (p = accept; *p != 0 ; p++)
+		{
+			if (c == *p)
+			{
+				return (s);
+			}
+		}
+	}
+	return (0);
+}
