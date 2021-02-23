233c2244ee276812257f182310411432009d94d3
toshi-uy <s_borgia@hotmail.com>
2021-02-23 18:52:49 +0000

    5

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -13,27 +13,29 @@ char *_strstr(char *haystack, char *needle)
 {
 	char *p1 = haystack;
 	char *p2 = needle;
-	int i = 0, j = 0, flag = 0, flag1 = 0;
+	int b, i = 0, j = 0;
 
-	for (i = 0; p1[i] != '\0'; i++)
+	while (*(p1 + i) != '\0')
 	{
-		if (p1[i] == p2[j])
-			flag = i;
-		for (j = 0; p2[j] != '\0'; j++)
+		if (*(p2 + j) == '\0')
+			break;
+		else if (*(p1 + i) == *(p2 + j))
 		{
-			if (p1[i] != p2[j])
-			{
-				flag1 = 0;
-				break;
-			}
-			if (p1[i] == p2[j])
-			{
-				flag1 = 1;
-				i++;
-			}
-			if (flag1 == 1)
-				return (haystack + flag);
+			i++;
+			j++;
+			b = 1;
+		}
+		else
+		{
+			i++;
+			b = 0;
+			j = 0;
 		}
 	}
-	return ('\0');
+
+	if (b == 0)
+		return ('\0');
+	else if (b == 1)
+		return (p1 + i - j);
+	return (0);
 }
