e5cda5b85c2a145250f60f49fe0d9e343482dcd5
toshi-uy <s_borgia@hotmail.com>
2021-02-23 16:09:27 +0000

    2

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -15,10 +15,13 @@ char *_strchr(char *s, char c)
 	char *str = s;
 
 	i = 0;
+
 	for (i = 0 ; str[i] != '\0' ; i++)
 	{
 		if (str[i] == c)
 			return (s + i);
 	}
+	if (str[i] == c)
+		return (s + i);
 	return (0);
 }
