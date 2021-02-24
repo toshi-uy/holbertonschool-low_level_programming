26327b8b2f117634c8e0cfea78f2b8faf58080db
toshi-uy <s_borgia@hotmail.com>
2021-02-24 18:04:28 +0000

    4

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,23 @@
+#include "holberton.h"
+
+/**
+ * _pow_recursion - x raised to the power of y
+ * @x: destination parameter
+ * @y: destination parameter
+ *
+ * Return: value of x raised to the power of y
+ *
+ */
+
+int _pow_recursion(int x, int y)
+{
+	if (y < 0)
+		return (-1);
+	if (y == 0)
+		return (1);
+	if (y > 0)
+	{
+		return (_pow_recursion(x, y - 1) * x);
+	}
+	return (0);
+}
