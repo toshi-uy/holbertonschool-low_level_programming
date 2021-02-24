a036db574e8eb5fc9cfd2afaf3b8a7641399f1c7
toshi-uy <s_borgia@hotmail.com>
2021-02-24 17:17:20 +0000

    2

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,17 @@
+B#include "holberton.h"
+
+/**
+ * _strlen_recursion - returns the length of a string
+ * @s: destination parameter
+ *
+ * Return: a pointer to the resulting string
+ *
+ */
+
+int _strlen_recursion(char *s)
+{
+	if (*s == '\0')
+		return (0);
+	else
+		return (_strlen_recursion(s + 1) + 1);
+}
