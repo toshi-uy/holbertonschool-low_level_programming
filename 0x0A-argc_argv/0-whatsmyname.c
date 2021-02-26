afba67050bf9990fc361b03e9e2dea4da86d4f69
toshi-uy <s_borgia@hotmail.com>
2021-02-26 18:46:09 +0000

    task 0 first try

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,15 @@
+#include "holberton.h"
+/**
+ * main - Entry point
+ * @argc: destination parameter
+ * @argv: parameter
+ *
+ * Return: success
+ *
+ */
+
+int main (int argc __attribute__((unused)), char *argv[])
+{
+	printf("%s\n", argv[0]);
+	return 0;
+}
