c11c25706c1e7d50b010186220ea4e97bd6a8e26
toshi-uy <s_borgia@hotmail.com>
2021-02-28 13:52:11 +0000

    first exercise

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,15 @@
+#include "holberton.h"
+/**
+ * main - Entry point
+ * @argc: destination parameter
+ * @argv: parameter
+ *
+ * Return: success
+ *
+ */
+
+int main (int argc, char *argv[]__attribute__((unused)))
+{
+	printf("%d\n", argc - 1);
+	return (0);
+}
