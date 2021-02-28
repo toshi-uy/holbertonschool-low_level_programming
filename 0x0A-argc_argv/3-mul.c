ab8295bc3a7b01149cc9a5b4f2d10e839d1fcd49
toshi-uy <s_borgia@hotmail.com>
2021-02-28 14:59:47 +0000

    project 3

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,25 @@
+#include "holberton.h"
+
+/**
+ * main - Entry point
+ * @argc: destination parameter
+ * @argv: parameter
+ *
+ * Return: success
+ *
+ */
+
+int main(int argc, char *argv[])
+{
+	int count, mul = 1;
+
+	if (argc < 3)
+	{
+		printf("Error\n");
+		return (1);
+	}
+	for (count = 1 ; count < argc ; count++)
+	mul = mul * atoi(argv[count]);
+	printf("%d\n", mul);
+	return (0);
+}
