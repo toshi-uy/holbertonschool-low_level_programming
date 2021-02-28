130337b0c3f35837ed2a63d94b8ea577764eaeaf
toshi-uy <s_borgia@hotmail.com>
2021-02-28 13:32:52 +0000

    betty fix

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -8,8 +8,8 @@
  *
  */
 
-int main (int argc __attribute__((unused)), char *argv[])
+int main(int argc __attribute__((unused)), char *argv[])
 {
 	printf("%s\n", argv[0]);
-	return 0;
+	return (0);
 }
