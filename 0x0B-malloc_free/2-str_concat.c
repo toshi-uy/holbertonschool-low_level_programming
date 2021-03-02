9514e469515c820b1bfb23332453a356b042b283
toshi-uy <s_borgia@hotmail.com>
2021-03-02 18:53:32 +0000

    2

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -35,13 +35,13 @@ char *str_concat(char *s1, char *s2)
 	int i = 0, j = 0;
 	char *p;
 
+	if (s1 == NULL)
+                s1 = "";
+        if (s2 == NULL)
+                s2 = "";
 	p = malloc(sizeof(char) * (_strlen(s1) + _strlen(s2) + 1));
 	if (p == NULL)
 		return (NULL);
-	if (s1 == NULL)
-		s1 = "";
-	if (s2 == NULL)
-		s2 = "";
 	for (i = 0; s1[i] != '\0'; i++)
 	{
 		p[i] = s1[i];
