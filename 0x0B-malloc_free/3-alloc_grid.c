18a72840acf887a88c0088afd360133a7973b653
toshi-uy <s_borgia@hotmail.com>
2021-03-02 20:44:30 +0000

    3

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -11,19 +11,26 @@
 
 int **alloc_grid(int width, int height)
 {
-	int i;
+	int i, j;
 	int **array;
 
 	if (width <= 0 || height <= 0)
 		return (NULL);
-	array = malloc(width * sizeof(int *));
+	array = malloc(height * sizeof(int *));
 	if (array == NULL)
 		return (NULL);
-	for (i = 0; i < width; i++)
+	for (i = 0 ; i < height ; i++)
 	{
-		array[i] = malloc(height * sizeof(int));
-	}
-	free(array);
-	free(array[i]);
+			array[i] = malloc(width * sizeof(int));
+			if (array[i] == NULL)
+			{
+				for (j = 0 ; j < i ; j++)
+					free(array[i]);
+				free(array);
+				return (NULL);
+			}
+			for (j = 0 ; j < width ; j++)
+				array[i][j] = 0;
+		}
 	return (array);
 }
