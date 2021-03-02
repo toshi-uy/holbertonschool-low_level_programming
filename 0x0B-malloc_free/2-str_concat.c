191a5d9bf43a8f0cd8937c12a7f038c5c87d74f5
toshi-uy <s_borgia@hotmail.com>
2021-03-02 17:58:18 +0000

    2

---
+++ 0x0B-malloc_free/2-str_concat.c
@@ -0,0 +1,51 @@
+#include "holberton.h"
+
+/**
+ * _strlen - counts strings
+ * @s: parameter s
+ *
+ * Description: counts string characters
+ *
+ * Return: Count
+ */
+
+int _strlen(char *s)
+{
+	int count = 0;
+
+	while (*(s + count))
+	{
+		count++;
+	}
+	return (count);
+}
+
+/**
+ * str_concat - concatenates two strings
+ * @s1: parameter s1
+ * @s2: parameter s2
+ *
+ * Description: function that concatenates two strings
+ *
+ * Return: point to a newly allocated space in memory
+ */
+
+char *str_concat(char *s1, char *s2)
+{
+	int i, j;
+	char *p;
+
+	p = malloc(sizeof(char) * (_strlen(s1) + _strlen(s2) + 1));
+	if (p == NULL)
+		return (NULL);
+	for (i = 0; s1[i] != '\0'; i++)
+	{
+		p[i] = s1[i];
+	}
+	for (j = 0; s2[j] != '\0'; j++)
+	{
+		p[i + j] = s2[j];
+	}
+	p[i + j] = '\0';
+	return (p);
+}
