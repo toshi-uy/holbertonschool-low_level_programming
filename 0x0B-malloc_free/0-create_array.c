9227eb6c197e41c18dc0c4e54ddd74d916856929
toshi-uy <s_borgia@hotmail.com>
2021-03-02 16:11:13 +0000

    fix betty and return null if fails

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -1,10 +1,10 @@
 #include "holberton.h"
 /**
- * main - Entry point
- * @argc: destination parameter
- * @argv: parameter
+ * create_array - Entry point
+ * @size: parameter
+ * @c: parameter
  *
- * Return: success
+ * Return: a pointer to the array.
  *
  */
 
@@ -16,6 +16,8 @@ char *create_array(unsigned int size, char c)
 	if (size == 0)
 		return (NULL);
 	p = malloc(sizeof(char) * size);
+	if (p == NULL)
+		return (NULL);
 	for (i = 0 ; i < size ; i++)
 		p[i] = c;
 	return (p);
