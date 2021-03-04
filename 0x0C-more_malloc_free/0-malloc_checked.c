34a6424bf7cec39b10f3b7e145f72e0f476157d2
toshi-uy <s_borgia@hotmail.com>
2021-03-04 14:31:12 +0000

    first two projects, fixed seg fault

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,18 @@
+#include "holberton.h"
+/**
+ * malloc_checked - Entry point
+ * @b: parameter
+ *
+ * Return: a pointer to the array.
+ *
+ */
+
+void *malloc_checked(unsigned int b)
+{
+	void *p;
+
+	p = malloc(b);
+	if (p == NULL)
+		exit (98);
+	return (p);
+}
---
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -0,0 +1,54 @@
+#include "holberton.h"
+/**
+ * _strlen - counts strings
+ * @s: parameter s
+ *
+ * Description: counts string characters
+ *
+ * Return: Count
+ */
+
+unsigned int _strlen(char *s)
+{
+	int count = 0;
+
+	while (*(s + count))
+	{
+		count++;
+	}
+	return (count);
+}
+
+/**
+ * string_nconcat - Entry point
+ * @s1: parameter
+ * @s2: parameter
+ * @n: parameter
+ *
+ * Return: a pointer to the array.
+ *
+ */
+
+char *string_nconcat(char *s1, char *s2, unsigned int n)
+{
+	char *p;
+	unsigned int i = 0, j = 0;
+
+	if (s1 == NULL)
+		s1 = "";
+	if (s2 == NULL)
+		s2 = "";
+	p = malloc((_strlen(s1) + _strlen(s2) + 1) * sizeof(char));
+	if (p == NULL)
+		return (NULL);
+	for (i = 0 ; i < _strlen(s1) ; i++)
+		p[i] = s1[i];
+	if (n >= _strlen(s2))
+		n = _strlen(s2) + 1;
+	for (j = 0 ; j < n ; j++, i++)
+	{
+		p[i] = s2[j];
+	}
+	p[i] = '\0';
+	return (p);
+}
