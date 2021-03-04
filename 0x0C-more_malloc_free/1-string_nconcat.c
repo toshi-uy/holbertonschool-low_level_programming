f368b04e6ab9b8415b82be367aedc8a4d6b8f110
toshi-uy <s_borgia@hotmail.com>
2021-03-04 16:39:36 +0000

    testing change

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -45,10 +45,10 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 		p[i] = s1[i];
 	if (n >= _strlen(s2))
 		n = _strlen(s2) + 1;
-	for (j = 0 ; j < n ; j++, i++)
+	for (j = 0 ; j < n ; j++)
 	{
-		p[i] = s2[j];
+		p[i + j] = s2[j];
 	}
-	p[i] = '\0';
+	p[i + j] = '\0';
 	return (p);
 }
