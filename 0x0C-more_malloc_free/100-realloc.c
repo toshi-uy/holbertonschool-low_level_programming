89f44769f33957e0d63f303c0761f960fbfb8230
toshi-uy <s_borgia@hotmail.com>
2021-03-04 23:05:46 +0000

    100 fixed ptr error deference

---
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -0,0 +1,43 @@
+#include "holberton.h"
+
+/**
+ * array_range - Entry point
+ * @min: parameter
+ * @max: parameter
+ *
+ * Description: function that creates an array of integers
+ *
+ * Return: the pointer to the newly created array
+ */
+
+void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
+{
+	char *new;
+	unsigned int i;
+	char *PTR = ptr;
+
+	if (!PTR)
+	{
+		new = malloc(new_size);
+		if (new == NULL)
+			return (NULL);
+		return (new);
+	}
+	if (new_size == old_size)
+		return (PTR);
+	if (new_size == 0 && PTR != NULL)
+	{
+		free(PTR);
+		return (NULL);
+	}
+	if (new_size > old_size)
+	{
+		new = malloc(new_size);
+		if (new == NULL)
+                        return (NULL);
+		for (i = 0; i < old_size; i++)
+			new[i] = PTR[i];
+		free(PTR);
+	}
+	return (new);
+}
