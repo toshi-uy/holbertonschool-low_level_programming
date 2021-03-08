472fa78a6e2352d352613412576b17da2172d451
toshi-uy <s_borgia@hotmail.com>
2021-03-08 13:28:24 +0000

    added new line

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: prints file name
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	printf("%s\n", __FILE__);
+	return (0);
+}
