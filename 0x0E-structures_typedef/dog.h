5a69a9092264c995290777229a99baaaf4db98dc
toshi-uy <s_borgia@hotmail.com>
2021-03-08 16:27:43 +0000

    dog structure

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,19 @@
+#ifndef DOG
+#define DOG
+/**
+ * struct dog - structure for dog
+ * @name: First member
+ * @owner: Second member
+ * @age: third member
+ *
+ * Description: structure used to descrube name owner and age of dog
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+
+}
+
+#endif
