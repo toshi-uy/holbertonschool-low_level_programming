9ee113c0f75ce80eef6bafe704338bccd8efff94
toshi-uy <s_borgia@hotmail.com>
2021-03-08 17:33:40 +0000

    dog

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -16,5 +16,13 @@ struct dog
 	char *owner;
 
 };
+/**
+ * dog_t - Typedef for structure dog
+ */
+typedef char dog_t;
+dog_t *new_dog(char *name, float age, char *owner);
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+void free_dog(dog_t *d);
 
 #endif
