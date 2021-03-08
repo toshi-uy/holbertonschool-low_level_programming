ffef026650305dac8e93a91492be2e67ab3254a0
toshi-uy <s_borgia@hotmail.com>
2021-03-08 18:11:32 +0000

    fix float

---
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "dog.h"
+
+/**
+ * print_dog - entry point
+ * @d: dog parameter.
+ *
+ */
+
+void print_dog(struct dog *d)
+{
+	if (d == NULL)
+		exit (1);
+	if((*d).name == NULL)
+		(*d).name = "(nil)";
+	if((*d).owner == NULL)
+                (*d).owner = "(nil)";
+printf("Name: %s\nAge: %.6f\nOwner: %s\n", (*d).name, (*d).age, (*d).owner);
+}
