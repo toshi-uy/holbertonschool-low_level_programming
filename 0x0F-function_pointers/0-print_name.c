00f5310a056fafc0e68c0397dcee071a03ba15bc
toshi-uy <s_borgia@hotmail.com>
2021-03-10 15:52:38 +0000

    fixed if

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,14 @@
+#include "function_pointers.h"
+
+/**
+ * print_name - prints a name
+ * @name: name of the person
+ *
+ * Return: Nothing.
+ */
+
+void print_name(char *name, void (*f)(char *))
+{
+	if (name && f)
+		(*f)(name);
+}
