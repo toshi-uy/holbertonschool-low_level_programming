7d9c862ec071ba43c10a80d20dffc3f133647460
toshi-uy <s_borgia@hotmail.com>
2021-03-10 17:47:51 +0000

    fixed if

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -13,15 +13,16 @@ int int_index(int *array, int size, int (*cmp)(int))
 {
 	int i, a;
 
-	if (size <= 0)
-		return (-1);
-	for (i = 0; i < size; i++)
+	if (cmp && array && size >0)
 	{
-		a = cmp(array[i]);
-		if (a)
-			break;
-	}
+		for (i = 0; i < size; i++)
+		{
+			a = cmp(array[i]);
+			if (a)
+				break;
+		}
 	if (a < size)
 		return (i);
+	}
 	return (-1);
 }
