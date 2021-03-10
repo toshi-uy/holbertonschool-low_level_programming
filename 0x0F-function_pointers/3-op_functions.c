3a9337f80da3d2b65a060e9ddba2e74f9653542c
toshi-uy <s_borgia@hotmail.com>
2021-03-10 21:09:31 +0000

    fixed all error and segmentation fault due to i not initialized

---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,70 @@
+#include "3-calc.h"
+/**
+ * op_add - Entry point
+ *
+ * @a: parameter a
+ * @b:parameter b
+ *
+ * Return: sum
+ */
+
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ * op_sub - Entry point
+ *
+ * @a: parameter a
+ * @b:parameter b
+ *
+ * Return: difference.
+ */
+
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+ * op_mul - Entry point
+ *
+ * @a: parameter a
+ * @b:parameter b
+ *
+ * Return: product.
+ */
+
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+ * op_div - Entry point
+ *
+ * @a: parameter a
+ * @b:parameter b
+ *
+ * Return: division.
+ */
+
+int op_div(int a, int b)
+{
+	return (a / b);
+}
+
+/**
+ * op_mod - Entry point
+ *
+ * @a: parameter a
+ * @b:parameter b
+ *
+ * Return: remainder of the division.
+ */
+
+int op_mod(int a, int b)
+{
+	return (a % b);
+}
