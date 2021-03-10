18731aa9d30ebc07dffb42b1325cf6d715adb779
toshi-uy <s_borgia@hotmail.com>
2021-03-10 16:18:44 +0000

    fixed for array[i]

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,19 @@
+#include "function_pointers.h"
+
+/**
+ * array_iterator - executes a function given as a parameter on each element
+ * @array: pointer to array
+ * @size: size of array
+ * @action: pointer to function
+ *
+ * Return: Nothing.
+ */
+
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	size_t i;
+
+	if (array && action)
+		for (i = 0 ; i < size ; i++)
+			(*action)(array[i]);
+}
