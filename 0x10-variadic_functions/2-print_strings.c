f3a17b1375d3d8cb70ddedf49af127a6e5b7b88b
toshi-uy <s_borgia@hotmail.com>
2021-03-11 19:28:55 +0000

    fixed va_arg++

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -10,25 +10,22 @@
 void print_strings(const char *separator, const unsigned int n, ...)
 {
 	unsigned int i;
+	char* chequeo;
 	va_list print_string;
 
 	va_start(print_string, n);
+	chequeo = va_arg(print_string, char*);
 	for (i = 0; i < n; i++)
 	{
+		if (chequeo != NULL)
+			printf("%s", chequeo);
+		else
+			printf("(nil)");
 		if (separator && i != (n - 1))
 		{
-			if (va_arg(print_string, char *))
-				printf("%s%s", va_arg(print_string, char *), separator);
-			else
-				printf("(nil)%s", separator);
+			printf("%s", separator);
 		}
-		else
-		{
-			if (va_arg(print_string, char *))
-				printf("%s", va_arg(print_string, char *));
-			else
-				printf("(nil)");
-		}
+		chequeo = va_arg(print_string, char*);
 	}
 	printf("\n");
 	va_end(print_string);
