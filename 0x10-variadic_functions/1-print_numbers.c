58fd39af97e07e12152f7b78a8b69e521190472d
toshi-uy <s_borgia@hotmail.com>
2021-03-11 17:50:30 +0000

    1

---
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -0,0 +1,24 @@
+#include "variadic_functions.h"
+
+/**
+ * print_numbers - Entry point
+ * @separator: pointer to char
+ * @n: constant number.
+ *
+ * Return nothing.
+ */
+
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+	unsigned int i;
+	va_list print_char;
+
+	va_start(print_char, n);
+	if (separator)
+	{
+		for (i = 0; i < n -1; i++)
+			printf("%d%s", va_arg(print_char, int), separator);
+		printf("%d\n", va_arg(print_char, int));
+	}
+	va_end(print_char);
+}
