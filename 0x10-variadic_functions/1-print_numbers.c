43c0bd4996e8445de949846e4d6920e2b752b834
toshi-uy <s_borgia@hotmail.com>
2021-03-11 18:17:52 +0000

    fixed for condition

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -22,7 +22,7 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 	}
 	else
 	{
-		for (i = 1; i <= n; i++)
+		for (i = 0; i <= n; i++)
 			printf("%d", va_arg(print_char, int));
 		printf("\n");
 	}
