255babcc01ddd5c1a2a8535353578816db88ac72
toshi-uy <s_borgia@hotmail.com>
2021-03-12 02:30:00 +0000

    fixed parameter chequeo and char print, changed char for int@

---
+++ 0x10-variadic_functions/3-print_all.c
@@ -0,0 +1,86 @@
+#include "variadic_functions.h"
+
+/**
+ * print_char - prints chars
+ * @print: parameter print.
+ * Return nothing
+ */
+
+void print_char(va_list print)
+{
+	printf("%c", va_arg(print, int));
+}
+
+/**
+ * print_int - prints integrer
+ * @print: parameter print.
+ * Return nothing
+ */
+
+void print_int(va_list print)
+{
+        printf("%d", va_arg(print, int));
+}
+
+/**
+ * print_float - prints floats
+ * @print: parameter print.
+ * Return nothing
+ */
+
+void print_float(va_list print)
+{
+	printf("%f", va_arg(print, double));
+}
+
+/**
+ * print_string - prints string
+ * @print: parameter pritn
+ * Return nothing
+ */
+
+void print_string(va_list print)
+{
+	char *chequeo;
+
+	chequeo = va_arg(print, char*);
+	if (chequeo == NULL)
+	{
+		printf("(nil)");
+	}
+	printf("%s", chequeo);
+}
+
+void print_all(const char * const format, ...)
+{
+	int i = 0, j = 0;
+	va_list print;
+	char *seperator;
+	opt options[] = {
+		{"c", print_char},
+		{"i", print_int},
+		{"f", print_float},
+		{"s", print_string},
+		{NULL, NULL}
+	};
+
+	va_start(print, format);
+	seperator = "";
+	while (format && format[i])
+	{
+		j = 0;
+		while (options[j].c != NULL)
+		{
+			if (format[i] == options[j].c[0])
+			{
+				printf("%s", seperator);
+				options[j].function(print);
+				seperator = ", ";
+			}
+			j++;
+		}
+		i++;
+	}
+	printf("\n");
+	va_end(print);
+}
--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -3,9 +3,25 @@
 #include <stdarg.h>
 #include <stdlib.h>
 #include <stdio.h>
+
 int _putchar(char c);
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
 void print_all(const char * const format, ...);
+void print_char(va_list argument);
+void print_int(va_list argument);
+void print_float(va_list argument);
+void print_string(va_list argument);
+
+/**
+ *struct op - struct op
+ *@c: string
+ *@function: pointer to function
+ */
+typedef struct op
+{
+	char *c;
+	void (*function)();
+} opt;
 #endif
