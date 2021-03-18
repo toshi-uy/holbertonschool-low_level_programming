dcd44e40343518b7a342948de7457baff864c90a
toshi-uy <s_borgia@hotmail.com>
2021-03-18 19:49:24 +0000

    fixed (nill) requirement

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -1,18 +1,21 @@
 #include "lists.h"
 
 /**
- * print_list - check the code for Holberton School students.
- *
+ * print_list - function that prints all the elements of a list_t
+ * @h - pointer to list
  * Return: count.
  */
 size_t print_list(const list_t *h)
 {
 	int count = 0;
 
-	while(h)
+	while (h)
 	{
+		if (h->str == NULL)
+			printf("[0] (nil)\n");
+		else
+			printf("[%d] %s\n", h->len, h->str);
 		count++;
-		printf("[%d] %s\n", h->len, h->str);
 		h = h->next;
 	}
 	return (count);
