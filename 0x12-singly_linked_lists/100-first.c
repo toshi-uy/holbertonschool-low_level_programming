cbecf667b1c3dbb6dc3ba64549832851f2378c82
toshi-uy <s_borgia@hotmail.com>
2021-03-19 19:36:06 +0000

    added a variable to fix betty, added new line after declaration

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -4,7 +4,10 @@
  * Return: Nothing
  */
 
-void previous (void)
+void previous(void)
 {
-        printf ("You're beat! and yet, you must allow,\nI bore my house upon my back!\n");
+	char *s;
+
+	s = "You're beat! and yet, you must allow,\n I bore my house upon my back!";
+		printf("%s\n", s);
 }
