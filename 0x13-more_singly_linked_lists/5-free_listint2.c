41605b609b4f7c286d72f3e6e5e75ab3487b59ba
toshi-uy <s_borgia@hotmail.com>
2021-03-22 16:23:31 +0000

    free list project

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+
+/**
+ * free_listint2 - function that frees a list
+ * @head: pointer to list
+ *
+ * Return: nothing
+ */
+
+void free_listint2(listint_t **head)
+{
+	listint_t *tmp;
+
+	while (*head)
+	{
+		tmp = *head;
+		*head = (*head)->next;
+		free(tmp);
+	}
+}
