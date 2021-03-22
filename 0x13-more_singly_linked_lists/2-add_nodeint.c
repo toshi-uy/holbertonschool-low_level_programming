6b007216d66fa23e020955e04cc02104bba8ca10
toshi-uy <s_borgia@hotmail.com>
2021-03-22 14:21:15 +0000

    adding 2nd project

---
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+/**
+ * print_listint - function that prints all the elements of a list
+ * @head: head pointer.
+ * @n: parameter n.
+ *
+ * Return: the address of the new element, or NULL if it failed
+ */
+
+listint_t *add_nodeint(listint_t **head, const int n)
+{
+	listint_t *current = *head;
+
+	current = malloc(sizeof(listint_t));
+	if (current == NULL)
+		return (NULL);
+	current->n = n;
+	current->next = *head;
+	*head = current;
+	return (*head);
+}
