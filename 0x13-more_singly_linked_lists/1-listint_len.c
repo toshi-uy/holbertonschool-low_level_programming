078238853ed9f00a9bbd8a032abc8c98a4a1f36a
toshi-uy <s_borgia@hotmail.com>
2021-03-22 13:20:47 +0000

    added project 1

---
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+
+/**
+ * listint_len - function that returns the number of elements in a linked list
+ * @h: h parameter.
+ *
+ * Return: the number of nodes
+ */
+
+size_t listint_len(const listint_t *h)
+{
+	unsigned int count;
+
+	while (h)
+	{
+		count++;
+		h = h->next;
+	}
+	return (count);
+}
