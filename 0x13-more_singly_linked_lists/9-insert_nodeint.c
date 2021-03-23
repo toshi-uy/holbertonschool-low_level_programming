62502c050b02bb0c6743435ed5f784f9a4ff9c4b
toshi-uy <s_borgia@hotmail.com>
2021-03-23 17:49:03 +0000

    9 betty fix and seg fault fix

---
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -0,0 +1,36 @@
+#include "lists.h"
+
+/**
+ * insert_nodeint_at_index - function that inserts a new node at a position
+ * @head: double pointer to list
+ * @idx: index
+ * @n: data parameter n.
+ *
+ * Return: the address of the new node, or NULL if it failed
+ */
+
+listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
+{
+	listint_t *aux, *new;
+	unsigned int count = 0;
+
+	if (!head)
+		return (0);
+	aux = *head;
+	while (aux)
+	{
+		if (idx - 1 == count)
+		{
+			new = malloc(sizeof(listint_t));
+			if (new == NULL)
+				return (NULL);
+			new->n = n;
+			new->next = aux->next;
+			aux->next = new;
+			return (new);
+		}
+		count++;
+		aux = aux->next;
+	}
+	return (NULL);
+}
