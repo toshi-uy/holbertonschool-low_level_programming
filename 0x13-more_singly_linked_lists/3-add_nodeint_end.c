3af5064bfcf381da9fd8831af1a75dd77b10c3b2
toshi-uy <s_borgia@hotmail.com>
2021-03-23 14:47:09 +0000

    fixed betty

--- 0x13-more_singly_linked_lists/3-add_nodeint_end.c
+++ 0x13-more_singly_linked_lists/3-add_nodeint_end.c
@@ -23,9 +23,9 @@ listint_t *add_nodeint_end(listint_t **head, const int n)
 		return (end);
 	}
 	while (current->next)
-        {
-                current = current->next;
-        }
+	{
+		current = current->next;
+	}
 	current->next = end;
 	return (end);
 }
