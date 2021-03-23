46d448357de766328d15aaa9e3ff7c8c2b36ad4b
toshi-uy <s_borgia@hotmail.com>
2021-03-23 18:42:02 +0000

    border case

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -24,7 +24,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 			return (NULL);
 		new->n = n;
 		new->next = aux;
-		*head = new;
+		new = *head;
 	}
 	while (aux)
 	{
