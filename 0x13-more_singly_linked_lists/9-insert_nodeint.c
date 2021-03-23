effa56bdab590166f8f122b96e694d2af9bf29a6
toshi-uy <s_borgia@hotmail.com>
2021-03-23 18:33:46 +0000

    border case

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -17,14 +17,14 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	if (!head)
 		return (NULL);
 	aux = *head;
-	if (idx - 1 == 0)
-	{		new = malloc(sizeof(listint_t));
+	if (idx == 0)
+	{
+		new = malloc(sizeof(listint_t));
 		if (new == NULL)
 			return (NULL);
 		new->n = n;
-		new->next = aux->next;
-		aux->next = new;
-		return (new);
+		new->next = NULL;
+		head->next = new;
 	}
 	while (aux)
 	{
