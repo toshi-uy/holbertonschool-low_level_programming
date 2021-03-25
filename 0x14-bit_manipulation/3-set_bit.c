0aee78ac56e88eacdb674b4cbe12ed10cc616c93
toshi-uy <s_borgia@hotmail.com>
2021-03-25 21:42:35 +0000

    fixed return 1 condition

---
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -0,0 +1,19 @@
+#include "holberton.h"
+
+/**
+ * set_bit - function that sets the value of a bit to 1 at a given index
+ * @n: pointer to int
+ * @index: position to change.
+ *
+ * Return: 1 if it worked, or -1 if an error occurred.
+ */
+
+int set_bit(unsigned long int *n, unsigned int index)
+{
+	unsigned long int mask = (1 << index);
+
+	if (index > 63)
+		return (-1);
+	*n = (*n & ~mask) | (1 << index);
+	return (1);
+}
