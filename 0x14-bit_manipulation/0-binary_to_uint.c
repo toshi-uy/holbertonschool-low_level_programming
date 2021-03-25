01ae1fa0fde9a6da84c258e0018fcad1bc2adf0e
toshi-uy <s_borgia@hotmail.com>
2021-03-25 17:16:22 +0000

    fixed while b isn't 1 or 0

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -15,7 +15,7 @@ unsigned int binary_to_uint(const char *b)
 	if (!b)
 		return (0);
 	for (i = 0; b[i] != '\0'; i++)
-		if (b[i] != '0' || b[i] != '1')
+		if (b[i] != '0' && b[i] != '1')
 			return (0);
 	for (num = 0; *b; b++)
 	{
