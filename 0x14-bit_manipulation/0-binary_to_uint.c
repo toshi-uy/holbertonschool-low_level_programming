9408e24e7159b1a0fbc434aa83f3d0d9fdce5bca
toshi-uy <s_borgia@hotmail.com>
2021-03-25 16:16:52 +0000

    betty fix

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -23,7 +23,7 @@ unsigned int _strlen(char *s)
 
 /**
  * binary_to_uint - function that converts a binary number to an unsigned int
- * @b - pointing to a string of 0 and 1 chars.
+ * @b: pointing to a string of 0 and 1 chars.
  *
  * Return: the converted number, or 0 if fails.
  */
