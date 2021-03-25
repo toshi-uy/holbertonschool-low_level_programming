735edb4435dcec216093eb2c48a808985cecd7d9
toshi-uy <s_borgia@hotmail.com>
2021-03-25 16:15:29 +0000

    changed _strlen

---
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,51 @@
+#include "holberton.h"
+/**
+ * _strlen - counts strings
+ * @s: parameter s
+ *
+ * Description: counts string characters
+ *
+ * Return: Count
+ */
+
+unsigned int _strlen(char *s)
+{
+	int count = 0;
+
+	if (s == NULL)
+		return (0);
+	while (*(s + count))
+	{
+		count++;
+	}
+	return (count);
+}
+
+/**
+ * binary_to_uint - function that converts a binary number to an unsigned int
+ * @b - pointing to a string of 0 and 1 chars.
+ *
+ * Return: the converted number, or 0 if fails.
+ */
+
+unsigned int binary_to_uint(const char *b)
+{
+	unsigned int k = 1, num = 0, len;
+	int c;
+
+	if (!b)
+		return (0);
+	len = strlen(b);
+
+	for (c = len - 1; c >= 0; c--)
+	{
+		if (b[c] != '0' && b[c] != '1')
+			return (0);
+		if (b[c] == '1')
+		{
+			num += k;
+		}
+		k *= 2;
+	}
+	return (num);
+}
