0967f34ce2e089f2fad0bfee26cf15b9a95f5aba
toshi-uy <s_borgia@hotmail.com>
2021-03-25 20:00:12 +0000

    it's recursion baby

---
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -0,0 +1,14 @@
+#include "holberton.h"
+
+/**
+ * print_binary - function that prints the binary representation of a number
+ * @n: number to be printed
+ *
+ * Return: nothing.
+ */
+void print_binary(unsigned long int n)
+{
+	if (n > 1)
+		print_binary(n >> 1);
+	_putchar('0' + (n & 1));
+}
