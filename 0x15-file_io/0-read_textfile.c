45ef413f0314c046bb442993ae4ed8406fa83a35
toshi-uy <s_borgia@hotmail.com>
2021-03-29 19:19:37 +0000

    fix try 1000

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -32,15 +32,12 @@ ssize_t read_textfile(const char *filename, size_t letters)
 		close(fd);
 		return (0);
 	}
-	if (imp != -1)
+	ret = write(STDOUT_FILENO, buff, letters);
+	if (ret == -1)
 	{
-		ret = write(STDOUT_FILENO, buff, letters);
-		if (ret == -1)
-		{
-			free(buff);
-			close(fd);
-			return (0);
-		}
+		free(buff);
+		close(fd);
+		return (0);
 	}
 	free(buff);
 	close(fd);
