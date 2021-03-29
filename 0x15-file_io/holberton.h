d6541536b8c33335c441ca6dd45907ba9bf33c61
toshi-uy <s_borgia@hotmail.com>
2021-03-29 19:22:24 +0000

    holberton

--- 0x15-file_io/holberton.h
+++ 0x15-file_io/holberton.h
@@ -1,12 +1,12 @@
 #ifndef HOLBERTON_H
 #define HOLBERTON_H
 
-#include <stdio.h>
-#include <stdlib.h>
 #include <unistd.h>
+#include <stdio.h>
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <fcntl.h>
+#include <stdlib.h>
 
 int _putchar(char c);
 ssize_t read_textfile(const char *filename, size_t letters);
