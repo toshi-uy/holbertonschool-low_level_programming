c3e42bacbcc0d3afc1f1e43adb473f8c5f7a0924
toshi-uy <s_borgia@hotmail.com>
2021-03-29 23:51:18 +0000

    added 2 cases

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -12,7 +12,7 @@ int main(int argc, char **argv)
 {
 	char buffer[1024];
 	int file_from, file_to, cls, cls1;
-	int count = 1, count2 = 0;
+	int count = 1;
 
 	if (argc != 3)
 	{
@@ -33,12 +33,16 @@ int main(int argc, char **argv)
 	}
 	while ((count = read(file_from, buffer, sizeof(buffer))) > 0)
 	{
-		count2 = write(file_to, buffer, count);
-		if (count != count2)
+		if (write(file_to, buffer, count); != count)
 		{
 			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
 			exit(99);
 		}
+	}
+	if (count == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
+		exit(98);
 	}
 	cls = close(file_from);
 	cls1 = close(file_to);
