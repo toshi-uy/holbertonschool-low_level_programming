92ffab64d7df29883c27e6201ab7c5a6ccc3f31e
toshi-uy <s_borgia@hotmail.com>
2021-03-29 20:56:26 +0000

    added O_WRONLY

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -1,4 +1,25 @@
 #include "holberton.h"
+/**
+ * _strlen - counts strings
+ * @s: parameter s
+ *
+ * Description: counts string characters
+ *
+ * Return: Count
+ */
+
+unsigned int _strlen(char *s)
+{
+	int count = 0;
+
+	if (s == NULL)
+		return (0);
+	while (*(s + count))
+	{
+		count++;
+	}
+	return (count);
+}
 
 /**
  * append_text_to_file - append_text_to_file
@@ -11,18 +32,18 @@
 int append_text_to_file(const char *filename, char *text_content)
 {
 	int fd;
+	ssize_t ret = 0;
 
 	if (!filename)
 		return (-1);
 	fd = open(filename, O_WRONLY | O_APPEND);
 	if (fd == -1)
 		return (-1);
-	while (*text_content)
-	{
-		if (write(fd, text_content, 1) == -1)
-			return (-1);
-		text_content++;
-	}
+	if (!text_content)
+		return (1);
+	ret = write(fd, text_content, _strlen(text_content));
+	if (ret == -1)
+		return (-1);
 	close(fd);
 	return (1);
 }
