98bb50aba4fb1c1c21c625ffedf61eba7ad3eb61
toshi-uy <s_borgia@hotmail.com>
2021-03-29 17:57:35 +0000

    changed function

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,28 +1,6 @@
 #include "holberton.h"
 
 /**
- * _strlen - counts strings
- * @s: parameter s
- *
- * Description: counts string characters
- *
- * Return: Count
- */
-
-unsigned int _strlen(const char *s)
-{
-	int count = 0;
-
-	if (s == NULL)
-		return (0);
-	while (*(s + count))
-	{
-		count++;
-	}
-	return (count);
-	}
-
-/**
  * read_textfile - function that reads a text file and prints it to the SO
  * @filename: name of the file to read
  * @letters: number of letters it should read and print
@@ -40,7 +18,7 @@ ssize_t read_textfile(const char *filename, size_t letters)
 		return (0);
 	fd = open(filename, O_RDONLY);
 	if (fd == -1)
-		return (0);
+		return (-1);
 	buff = malloc(sizeof(char) * letters);
 	if (buff == NULL)
 		return (0);
@@ -51,7 +29,7 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	if (ret == -1)
 		return (-1);
 	}
+	free(buff);
 	close(fd);
-
 	return (imp);
 }
