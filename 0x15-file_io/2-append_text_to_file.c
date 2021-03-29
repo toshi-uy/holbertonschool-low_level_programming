64033ca717321b1d3ff6d869993f0445ddd6ef9f
toshi-uy <s_borgia@hotmail.com>
2021-03-29 20:54:59 +0000

    added O_WRONLY

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -14,7 +14,7 @@ int append_text_to_file(const char *filename, char *text_content)
 
 	if (!filename)
 		return (-1);
-	fd = open(filename, O_APPEND | O_RDWR);
+	fd = open(filename, O_WRONLY | O_APPEND);
 	if (fd == -1)
 		return (-1);
 	while (*text_content)
