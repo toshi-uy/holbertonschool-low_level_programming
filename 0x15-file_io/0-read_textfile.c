d0d9f6de779bc32f60484ce7aa236270cfafaee5
toshi-uy <s_borgia@hotmail.com>
2021-03-29 17:58:34 +0000

    changed function

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -18,7 +18,7 @@ ssize_t read_textfile(const char *filename, size_t letters)
 		return (0);
 	fd = open(filename, O_RDONLY);
 	if (fd == -1)
-		return (-1);
+		return (0);
 	buff = malloc(sizeof(char) * letters);
 	if (buff == NULL)
 		return (0);
