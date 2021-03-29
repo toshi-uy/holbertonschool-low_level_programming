502a2713be3ff7e88bce3e1851a6191d63de8c2d
toshi-uy <s_borgia@hotmail.com>
2021-03-29 20:20:57 +0000

    creat file

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -14,6 +14,8 @@ int create_file(const char *filename, char *text_content)
 
 	if (!filename)
 		return (-1);
+	if (!text_content)
+                return (-1);
 	fd = open(filename, O_CREAT | O_RDWR, 0600);
 	if (fd == -1)
 		return (-1);
