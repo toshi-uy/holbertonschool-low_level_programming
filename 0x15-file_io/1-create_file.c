853c2ea8a06b770278daba0b1e01adcff5ef474c
toshi-uy <s_borgia@hotmail.com>
2021-03-29 20:49:00 +0000

    added O_EXCL

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -37,7 +37,7 @@ int create_file(const char *filename, char *text_content)
 
 	if (!filename)
 		return (-1);
-	fd = open(filename, O_CREAT | O_RDWR, 0600);
+	fd = open(filename, O_CREAT | O_EXCL | O_RDWR, 0600);
 	if (fd == -1)
 		return (-1);
 	if (!text_content)
