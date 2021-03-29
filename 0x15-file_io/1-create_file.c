001df8d168fdd703b7b5e372123fc33dcccb2528
toshi-uy <s_borgia@hotmail.com>
2021-03-29 20:25:17 +0000

    fix

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -15,7 +15,7 @@ int create_file(const char *filename, char *text_content)
 	if (!filename)
 		return (-1);
 	if (!text_content)
-                return (-1);
+		return (-1);
 	fd = open(filename, O_CREAT | O_RDWR, 0600);
 	if (fd == -1)
 		return (-1);
