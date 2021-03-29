97e35c2f1820b208bdf4a2a658208eda16672a75
toshi-uy <s_borgia@hotmail.com>
2021-03-29 18:58:31 +0000

    same as before

---
+++ 0x15-file_io/2-append_text_to_file.c
@@ -0,0 +1,28 @@
+#include "holberton.h"
+
+/**
+ * append_text_to_file - append_text_to_file
+ * @filename: file to create
+ * @text_content: pointer to a text.
+ *
+ * Return: 1 if it works, -1 if anything fails.
+ */
+
+int append_text_to_file(const char *filename, char *text_content)
+{
+	int fd;
+
+	if (!filename)
+		return (-1);
+	fd = open(filename, O_APPEND | O_RDWR, 0600);
+	if (fd == -1)
+		return (-1);
+	while (*text_content)
+	{
+		if (write(fd, text_content, 1) == -1)
+			return (-1);
+		text_content++;
+	}
+	close(fd);
+	return (1);
+}
