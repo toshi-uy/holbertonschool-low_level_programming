e4e0fce85ebfa2970bc2ada1e98a53ea80044532
toshi-uy <s_borgia@hotmail.com>
2021-03-29 22:59:40 +0000

    still cant make it rw on group

---
+++ 0x15-file_io/3-cp.c
@@ -0,0 +1,42 @@
+#include "holberton.h"
+
+/**
+ * main - program that copies the content of a file to another file
+ *
+ * Return: Creates the new file with the content of the old one or exit.
+ */
+
+int main(int argc, char **argv)
+{
+	char buffer[1024];
+	int file_from, file_to, cls;
+	int count = 0;
+
+	if (argc != 3)
+	{
+		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
+		exit(97);
+	}
+	file_from = open(argv[1], O_RDONLY);
+	if (file_from == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
+		exit(98);
+	}
+	file_to = open(argv[2], O_WRONLY | O_TRUNC | O_CREAT, 0664);
+	if (file_to == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
+		exit(99);
+	}
+	while ((count = read(file_from, buffer, sizeof(buffer))) != 0)
+		write(file_to, buffer, count);
+	cls = close(file_from);
+	if (cls == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", cls);
+                exit(100);
+	}
+	close(file_to);
+	return (0);
+}
