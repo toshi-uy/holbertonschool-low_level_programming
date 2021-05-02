d7ca3deb4b001e0b15dc5c02fb1b14a3d6dd10ac
toshi-uy <s_borgia@hotmail.com>
2021-05-02 20:53:00 +0000

    added if aux->next null

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -35,7 +35,7 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 		}
 		return (new);
 	}
-	for (count = 0; aux->next != NULL || count + 1 == idx; count++)
+	for (count = 0; aux->next != NULL; count++)
 	{
 		if (count + 1 == idx)
 		{
@@ -46,11 +46,21 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 			new->prev = aux;
 			new->next = aux->next;
 			aux->next = new;
-			if (aux->next != NULL)
-				aux->next->prev = new;
+			aux->next->prev = new;
 			return (new);
 		}
 		aux = aux->next;
+	}
+	if (aux->next == NULL && count + 1 == idx)
+	{
+		new = malloc(sizeof(dlistint_t));
+		if (new == NULL)
+			return (NULL);
+		new->n = n;
+		new->prev = aux;
+		new->next = NULL;
+		aux->next = new;
+		return (new);
 	}
 	return (NULL);
 }
