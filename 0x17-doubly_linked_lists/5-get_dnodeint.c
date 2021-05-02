fd54e0b2e10056c9cff544e5f8eaeac943638ad7
toshi-uy <s_borgia@hotmail.com>
2021-05-02 17:34:06 +0000

    confused about excercise2

--- 0x17-doubly_linked_lists/5-get_dnodeint.c
+++ 0x17-doubly_linked_lists/5-get_dnodeint.c
@@ -2,46 +2,24 @@
 
 /**
  * add_dnodeint - function that adds a new node at the beginning of a list
- * @head: pointer to list
- * @n: pointer to data.
+ * @head: pointer to list head
+ * @index: pointer to index.
  *
- * Return: address of the new elemnt created
+ * Return: address of the nth node of a double linked list
  */
 
 dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
 {
-	dlistint_t *new, *aux;
-	unsigned int count = 0;
+	dlistint_t *aux = head;
 
 	if (!head)
+	{
 		return (NULL);
-	aux = head;
-	if (index == 0)
-	{
-		new = malloc(sizeof(dlistint_t));
-		if (new == NULL)
-			return (NULL);
-		new->n = head->n;
-		new->next = aux;
-		new->prev = NULL;
-		head = new;
-		return (new);
 	}
-	while (aux)
+	while (index != 0)
 	{
-		if (index - 1 == count)
-		{
-			new = malloc(sizeof(dlistint_t));
-			if (new == NULL)
-				return (NULL);
-			new->n = aux->next->n;
-			new->next = aux->next;
-			new->prev = aux->prev;
-			aux->next = new;
-			return (new);
-		}
-		count++;
 		aux = aux->next;
+		index--;
 	}
-	return (NULL);
+	return (aux);
 }
