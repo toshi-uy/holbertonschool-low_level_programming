8193567676a0f205c1f9bb9b11787d7b63ca88ff
toshi-uy <s_borgia@hotmail.com>
2021-05-02 18:01:49 +0000

    changed condition to idx != 1

---
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -0,0 +1,48 @@
+#include "lists.h"
+
+/**
+ * insert_dnodeint_at_index - function that inserts a node at a given position
+ * @h: pointer to list header
+ * @idx: index to insert
+ * @n: pointer to data.
+ *
+ * Return: address of the new elemnt created
+ */
+
+dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
+{
+	dlistint_t *new = NULL, *aux = *h, *aux2 = NULL;
+
+	new = malloc(sizeof(dlistint_t));
+	if (new == NULL)
+		return (NULL);
+	new->prev = NULL;
+	new->n = n;
+	new->next = NULL;
+	if (!h)
+	{
+		free(new);
+		return (NULL);
+	}
+	while (idx != 1)
+	{
+		aux = aux->next;
+		idx--;
+	}
+	if (aux->next == NULL)
+	{
+		aux->next = new;
+		new->prev = aux;
+	}
+	else
+	{
+		aux2 = aux->next;
+		aux->next = new;
+		aux2->prev = new;
+		new->next = aux2;
+		new->prev = aux;
+		return (new);
+	}
+	free(new);
+	return (NULL);
+}
