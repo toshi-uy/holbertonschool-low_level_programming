9d35eb9abcf08814a3ccf5a8558d2dd4d7b73659
toshi-uy <s_borgia@hotmail.com>
2021-05-02 20:18:57 +0000

    added case where *h = NULL

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -23,8 +23,10 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 		new->n = n;
 		new->prev = NULL;
 		if (*h == NULL)
+		{
 			new->next = NULL;
 			*h = new;
+		}
 		else
 		{
 		new->next = *h;
