f62de92828797bb4051025dce9f46a0f1f20bee3
toshi-uy <s_borgia@hotmail.com>
2021-05-27 16:56:33 -0300

    sadas

--- 0x1A-hash_tables/2-key_index.c
+++ 0x1A-hash_tables/2-key_index.c
@@ -15,7 +15,7 @@ unsigned long int key_index(const unsigned char *key, unsigned long int size)
 	unsigned long int index;
 
 	index = hash_djb2(key);
-	for (i = 0; i < size; i++)
-		
+
+
 	return (index % size);
 }
