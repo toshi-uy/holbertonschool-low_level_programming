06fcda9e9dd6663f05b7b688c3e585b0cfd7b0df
toshi-uy <s_borgia@hotmail.com>
2021-05-27 16:00:31 -0300

    fixed betty

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -9,23 +9,23 @@
 
 hash_table_t *hash_table_create(unsigned long int size)
 {
-    hash_table_t *hashtable = NULL;
-    unsigned int i;
+	hash_table_t *hashtable = NULL;
+	unsigned int i;
 
-    if (size < 1)
-        return NULL;
-    hashtable = malloc(sizeof(hash_table_t));
-    if (hashtable == NULL)
-        return NULL;
-    
-    hashtable->size = size;
-    hashtable->array = malloc(sizeof(hash_node_t) * size);
-    if (hashtable->array == NULL)
-    {
-        free(hashtable);
-        return NULL;
-    }
-    for( i = 0; i < size; i++ )
+	if (size < 1)
+		return NULL;
+	hashtable = malloc(sizeof(hash_table_t));
+	if (hashtable == NULL)
+		return NULL;
+
+	hashtable->size = size;
+	hashtable->array = malloc(sizeof(hash_node_t) * size);
+	if (hashtable->array == NULL)
+	{
+		free(hashtable);
+		return NULL;
+	}
+	for( i = 0; i < size; i++ )
 		hashtable->array[i] = NULL;
-    return hashtable;
+	return hashtable;
 }
---
+++ 0x1A-hash_tables/2-key_index.c
@@ -0,0 +1,15 @@
+#include "hash_tables.h"
+
+/**
+ * key_index - function that gives you the index of a key
+ * size: size of hash table
+ * key: pointer to key
+ *
+ * Return: the index at which the key/value pair should be stored 
+ * in the array of the hash table
+ */
+
+unsigned long int key_index(const unsigned char *key, unsigned long int size)
+{
+    
+}
