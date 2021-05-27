ed0b3958c1d2817213f5e80f4a92e54c582dcec6
toshi-uy <s_borgia@hotmail.com>
2021-05-27 15:37:48 -0300

    added blank line

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -19,8 +19,8 @@ hash_table_t *hash_table_create(unsigned long int size)
         return NULL;
     
     hashtable->size = size;
-    hashtable->array = malloc(sizeof(hash_node_t*) * size);
-    if (hashtable->array = NULL)
+    hashtable->array = malloc(sizeof(hash_node_t) * size);
+    if (hashtable->array == NULL)
     {
         free(hashtable);
         return NULL;
