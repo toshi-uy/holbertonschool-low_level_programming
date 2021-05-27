9cd92b3c9a934fb45963f8343a4c5961dd62d31a
toshi-uy <s_borgia@hotmail.com>
2021-05-27 16:11:41 -0300

    added file 2 and library to header

--- 0x1A-hash_tables/2-key_index.c
+++ 0x1A-hash_tables/2-key_index.c
@@ -2,14 +2,23 @@
 
 /**
  * key_index - function that gives you the index of a key
- * size: size of hash table
- * key: pointer to key
+ * @size: size of hash table
+ * @key: pointer to key
  *
- * Return: the index at which the key/value pair should be stored 
+ * Return: the index at which the key/value pair should be stored
  * in the array of the hash table
  */
 
 unsigned long int key_index(const unsigned char *key, unsigned long int size)
 {
-    
+	unsigned long int i = 0;
+	unsigned long int value;
+
+	while (value < ULONG_MAX && i < strlen(size))
+	{
+		value = value << 8;
+		value += key[i];
+		i++;
+	}
+	return (value % size);
 }
