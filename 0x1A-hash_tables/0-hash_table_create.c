03c7fa196763c64d9397a2537fd0713e87b2a962
toshi-uy <s_borgia@hotmail.com>
2021-05-28 17:06:29 -0300

    added comments to task 1

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -12,20 +12,27 @@ hash_table_t *hash_table_create(unsigned long int size)
 	hash_table_t *hashtable = NULL;
 	unsigned int i;
 
+	/* if size is less than one no hashtable would be created */
 	if (size < 1)
 		return (NULL);
+	/* alloc size for hash table and check if it's allocated */
 	hashtable = malloc(sizeof(hash_table_t));
 	if (hashtable == NULL)
 		return (NULL);
-
+	/* adding data to hashtable */
 	hashtable->size = size;
+	/* the size of the array would be the array * size of it */
 	hashtable->array = malloc(sizeof(hashtable->array) * size);
+	/* checking if it could be allocated */
 	if (hashtable->array == NULL)
 	{
+		/* if not we will have to free the hashtable created */
 		free(hashtable);
 		return (NULL);
 	}
+	/* for each array in the hash table assign the pointer to NULL */
 	for (i = 0; i < size; i++)
 		hashtable->array[i] = NULL;
+	/* return the created hash table */
 	return (hashtable);
 }
