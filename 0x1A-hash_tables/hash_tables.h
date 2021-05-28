101dfbad26dcecd79beb4d984b8789e552376394
toshi-uy <s_borgia@hotmail.com>
2021-05-28 14:14:25 -0300

    added 4 file and added header guard

--- 0x1A-hash_tables/hash_tables.h
+++ 0x1A-hash_tables/hash_tables.h
@@ -1,3 +1,6 @@
+#ifndef HASH_H
+#define HASH_H
+
 #include <stdarg.h>
 #include <stdio.h>
 #include <stdlib.h>
@@ -41,3 +44,5 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value);
 char *hash_table_get(const hash_table_t *ht, const char *key);
 void hash_table_print(const hash_table_t *ht);
 void hash_table_delete(hash_table_t *ht);
+
+#endif /* ends HASH_H */
