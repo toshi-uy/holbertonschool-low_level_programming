52b28b74a82bcdbc1dbc7200d3e9d053f85f5a1d
toshi-uy <s_borgia@hotmail.com>
2021-05-28 14:41:59 -0300

    addding comments

--- 0x1A-hash_tables/5-hash_table_print.c
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -12,16 +12,23 @@ void hash_table_print(const hash_table_t *ht)
 	hash_node_t *tmp;
 	unsigned long int i;
 
+	/* checking if there is no hash table */
 	if (!ht)
 		return;
+	/* printing the formating */
 	printf("{");
+	/* looping through the array */
 	for (i = 0; i < ht->size; i++)
 	{
+		/* in each index of the array set the pointer to the head */
 		tmp = ht->array[i];
+		/* loop thought the linked list */
 		while(tmp)
 		{
+			/* print the key followed by the value */
 			printf("'%s': '%s'", tmp->key, tmp->value);
 		}
 	}
+	/* print the end of the format and new line */
 	printf("}\n");
 }
