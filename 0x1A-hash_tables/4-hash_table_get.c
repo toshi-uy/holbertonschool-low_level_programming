08047dc9c203df043f255b0b9f782d47f1bb4074
toshi-uy <s_borgia@hotmail.com>
2021-05-28 14:19:53 -0300

    finished task

--- 0x1A-hash_tables/4-hash_table_get.c
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -13,6 +13,23 @@ char *hash_table_get(const hash_table_t *ht, const char *key)
 	unsigned long int index;
 	hash_node_t *tmp;
 
+	/* checking if the key or the node are null */
 	if (!key || !ht || *key == '\0')
 		return (NULL);
+		/* getting the index of the key */
+	index = key_index((const unsigned char *)key, ht->size);
+	/* creating pointer to head of the array */
+	tmp = ht->array[index];
+	/* looping in the array */
+	while (tmp)
+	{
+		/* checking if the key matches the key in the node */
+		if (strcmp(key, tmp->key) == 0)
+		{
+			/* if true return the value */
+			return (tmp->value);
+		}
+		tmp = tmp->next;
+	}
+	/* if no key was found return NULL */
 }
