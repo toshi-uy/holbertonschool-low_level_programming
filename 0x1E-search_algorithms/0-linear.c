6098cfa22c80bff42c5d557b7ccf29bae73053cf
toshi-uy <s_borgia@hotmail.com>
2021-08-30 13:39:54 -0300

    added parameters to comments

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -3,6 +3,9 @@
 /**
  * linear_search - function that searches for a value in an array of integers
  * using the Linear search algorithm
+ * @array: array to search in
+ * @size: size of the array
+ * @value: value to search for
  *
  * Return: the first index where value is located, or -1 if it fails
  */
