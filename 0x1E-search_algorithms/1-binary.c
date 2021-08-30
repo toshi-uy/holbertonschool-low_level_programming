204a02bc22efed511d2777773c1fd5404e0ff31d
toshi-uy <s_borgia@hotmail.com>
2021-08-30 15:09:48 -0300

    middle - 1

--- 0x1E-search_algorithms/1-binary.c
+++ 0x1E-search_algorithms/1-binary.c
@@ -23,7 +23,7 @@ int binary_search(int *array, size_t size, int value)
 	while (low <= high)
 	{
 		printf("Searching in array: ");
-		for (i = low; i < high - 1; i++)
+		for (i = low; i < high; i++)
 			printf("%d, ", array[i]);
 		printf("%d\n", array[high]);
 		if (array[mid] < value)
