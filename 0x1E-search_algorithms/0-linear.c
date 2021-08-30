569671066fc2d11b4caba3e08ee944d1003ce2b1
toshi-uy <s_borgia@hotmail.com>
2021-08-30 12:30:31 -0300

    added exercise to test

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -1,11 +1,26 @@
 #include "search_algos.h"
 
 /**
- * main - Entry point
+ * linear_search - function that searches for a value in an array of integers
+ * using the Linear search algorithm
  *
- * Return: Always EXIT_SUCCESS
+ * Return: the first index where value is located, or -1 if it fails
  */
+
 int linear_search(int *array, size_t size, int value)
 {
-    
+	size_t i = 0;
+
+	if (!array)
+		return (-1);
+	for (i = 0; i < size; i++)
+	{
+		printf("Value checked array[%i] = [%i]", i, array[i])
+		if array[i] == value
+		{
+		printf("Found %i at index: %i", value, i)
+		break;
+		}
+	} 
+	return (i);
 }
