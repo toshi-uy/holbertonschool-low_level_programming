8e5cc31c1ca024316697f04459054551f59cfad0
toshi-uy <s_borgia@hotmail.com>
2021-08-30 12:33:48 -0300

    commented advanced and added missing ;

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -16,7 +16,7 @@ int linear_search(int *array, size_t size, int value)
 		return (-1);
 	for (i = 0; i < size; i++)
 	{
-		printf("Value checked array[%i] = [%i]", i, array[i])
+		printf("Value checked array[%i] = [%i]", i, array[i]);
 		if (array[i] == value)
 		{
 			printf("Found %i at index: %i", value, i)
