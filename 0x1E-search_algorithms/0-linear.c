4d045440cf3ec48a7c43438297025c7f4276c665
toshi-uy <s_borgia@hotmail.com>
2021-08-30 12:12:54 -0300

    added header and file

---
+++ 0x1E-search_algorithms/0-linear.c
@@ -0,0 +1,11 @@
+#include "search_algos.h"
+
+/**
+ * main - Entry point
+ *
+ * Return: Always EXIT_SUCCESS
+ */
+int linear_search(int *array, size_t size, int value)
+{
+    
+}
---
+++ 0x1E-search_algorithms/search_algos.h
@@ -0,0 +1,22 @@
+#ifndef SEARCH_A
+#define SEARCH_A
+#include <stdarg.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <limits.h>
+
+/* Prototypes */
+int linear_search(int *array, size_t size, int value);
+int binary_search(int *array, size_t size, int value);
+
+/* Prototypes for advanced task */
+int jump_search(int *array, size_t size, int value);
+int interpolation_search(int *array, size_t size, int value);
+int exponential_search(int *array, size_t size, int value);
+int advanced_binary(int *array, size_t size, int value);
+listint_t *jump_list(listint_t *list, size_t size, int value);
+skiplist_t *linear_skip(skiplist_t *list, int value);
+
+
+#endif /* ends SEARCH_A */
