9c53340caef634ac705655639fac08d8838a639b
toshi-uy <s_borgia@hotmail.com>
2021-08-30 12:40:02 -0300

    removed print

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -19,10 +19,9 @@ int linear_search(int *array, size_t size, int value)
 		printf("Value checked array[%lu] = [%d]\n", i, array[i]);
 		if (array[i] == value)
 		{
-			printf("Found %d at index: %lu\n", value, i);
 			result = i;
 			break;
 		}
-	} 
+	}
 	return (result);
 }
