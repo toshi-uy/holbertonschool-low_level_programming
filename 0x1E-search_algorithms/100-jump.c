861078941615c50b5a0e5aed0d1c2d95878f5f15
toshi-uy <s_borgia@hotmail.com>
2021-08-31 17:25:02 -0300

    upload for tests

--- 0x1E-search_algorithms/100-jump.c
+++ 0x1E-search_algorithms/100-jump.c
@@ -1,7 +1,8 @@
 #include "search_algos.h"
+#include <math.h>
 
 /**
- * binary_search - function that searches for a value in an array of integers
+ * jump_search - function that searches for a value in an array of integers
  * using the Binary search algorithm
  * @array: array to search in
  * @size: size of the array
@@ -10,29 +11,33 @@
  * Return: the first index where value is located, or -1 if it fails
  */
 
-int binary_search(int *array, size_t size, int value)
+int jump_search(int *array, size_t size, int value)
 {
-	int high, mid, low, i = 0;
+	size_t jump, j = 0, i = 0;
 
 	if (!array)
 		return (-1);
-	low = 0;
-	high = size - 1;
-	mid = (low + high) / 2;
+	jump = sqrt(size);
 
-	while (low <= high)
+	while (j < size - 1)
 	{
-		printf("Searching in array: ");
-		for (i = low; i < high; i++)
-			printf("%d, ", array[i]);
-		printf("%d\n", array[high]);
-		if (array[mid] < value)
-			low = mid + 1;
-		else if (array[mid] == value)
-			return (value);
-		else
-			high = mid - 1;
-		mid = (low + high) / 2;
+		printf("Value checked array[%lu] = [%d]\n", j, array[j]);
+		if (array[j] == value)
+			printf("Value found between indexes [%lu] = [%lu]\n", j, (j + jump));
+			printf("Value checked array[%lu] = [%d]\n", j, array[j]);
+			return j;
+		if (array[j] != value && array[i] < value)
+			j += jump;
+		else if (array[j] != value && array[i] > value)
+		{
+			printf("Value found between indexes [%lu] = [%lu]\n", j, (j + jump));
+			for (i = j; i <= j + jump; i++)
+			{
+				printf("Value checked array[%lu] = [%d]\n", i, array[i]);
+				if (array[i] == value)
+					return i;
+			}
+		}
 	}
 	return (-1);
 }
