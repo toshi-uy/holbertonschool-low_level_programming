a1e850d6d553143b677741aa4a7779c260cdb792
toshi-uy <s_borgia@hotmail.com>
2021-09-01 10:47:04 -0300

    adden unnecesary print

--- 0x1E-search_algorithms/100-jump.c
+++ 0x1E-search_algorithms/100-jump.c
@@ -37,6 +37,6 @@ int jump_search(int *array, size_t size, int value)
 			}
 		}
 	}
-	printf("Value found between indexes [%lu] and [%lu]\n", j, j + jump);
+	printf("Value found between indexes [%lu] and [%lu]\n", j - jump, j);
 	return (-1);
 }
