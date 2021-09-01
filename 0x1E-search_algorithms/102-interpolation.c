a1947e9826d18afb8ec29981806182866bda6ad4
toshi-uy <s_borgia@hotmail.com>
2021-09-01 17:15:13 -0300

    added print to debut

--- 0x1E-search_algorithms/102-interpolation.c
+++ 0x1E-search_algorithms/102-interpolation.c
@@ -25,6 +25,7 @@ int interpolation_search(int *array, size_t size, int value)
 		}
 		pos = low +
 (((double)(high - low) / (array[high] - array[low])) * (value - array[low]));
+		printf("POS: %lu\n", pos);
 		if (pos > size)
 			printf("Value checked array[%lu] is out of range", pos);
 		else
