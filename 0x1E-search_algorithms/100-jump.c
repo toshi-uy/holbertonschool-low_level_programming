b0144556b876ba33aa13eac4e35495caea0a6180
toshi-uy <s_borgia@hotmail.com>
2021-09-01 09:55:07 -0300

    () on return

--- 0x1E-search_algorithms/100-jump.c
+++ 0x1E-search_algorithms/100-jump.c
@@ -33,7 +33,7 @@ int jump_search(int *array, size_t size, int value)
 			{
 				printf("Value checked array[%lu] = [%d]\n", i, array[i]);
 				if (array[i] == value)
-					return i;
+					return (i);
 			}
 		}
 	}
