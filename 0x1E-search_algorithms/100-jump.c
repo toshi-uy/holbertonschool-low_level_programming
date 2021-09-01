dd65b482a9efecb2d0c84967277d321647a1a8b6
toshi-uy <s_borgia@hotmail.com>
2021-09-01 16:45:58 -0300

    added else

--- 0x1E-search_algorithms/100-jump.c
+++ 0x1E-search_algorithms/100-jump.c
@@ -34,9 +34,8 @@ int jump_search(int *array, size_t size, int value)
 				printf("Value checked array[%lu] = [%d]\n", i, array[i]);
 				if (array[i] == value)
 					return (i);
-				else
-					return (-1);
 			}
+			return (-1);
 		}
 	}
 	printf("Value found between indexes [%lu] and [%lu]\n", j - jump, j);
