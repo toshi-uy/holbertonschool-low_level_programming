1014e7ad40ef376316a64a47fb3c8a73871d9a51
toshi-uy <s_borgia@hotmail.com>
2021-09-01 19:30:03 -0300

    added print to debug

--- 0x1E-search_algorithms/103-exponential.c
+++ 0x1E-search_algorithms/103-exponential.c
@@ -21,46 +21,5 @@ int exponential_search(int *array, size_t size, int value)
 		return (0);
 	while (i < size && array[i] <= value)
         	i = i * 2;
-	return (binary_search_rec(array, i / 2, minimum(i, size - 1), value));
-}
-
-/**
- * binary_search_rec - function that searches for a value in an array of integers
- * using the Binary search algorithm
- * @array: array to search in
- * @min: min of array
- * @max: max of array
- * @value: value to search for
- *
- * Return: the first index where value is located, or -1 if it fails
- */
-
-int binary_search_rec(int *array, size_t min, size_t max, int value)
-{
-	size_t mid = 0;
-
-	if (max >= min)
-	{
-		mid = min + (max - 1) / 2;
-		if (array[mid] == value)
-			return (mid);
-		if (array[mid] > value)
-			return (binary_search_rec(array, min, (mid - 1), value));
-	        return (binary_search_rec(array, mid + 1, max, value));
-	}
-	return(-1);
-}
-
-/**
- * minimum - minimum of two values
- * @first: first value to check
- * @second: second value to check
- * Return: the minimum value
- */
-
-size_t minimum(size_t first, size_t second)
-{
-	if (second < first)
-		return (second);
-	return (first);
+	return (binary_search(array, i / 2, value));
 }
