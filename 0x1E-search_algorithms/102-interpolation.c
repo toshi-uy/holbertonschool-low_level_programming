987be963e7cd58fe51cca1fa6e0cc2d1e4bb7ff6
toshi-uy <s_borgia@hotmail.com>
2021-09-01 18:49:07 -0300

    !array condition

--- 0x1E-search_algorithms/102-interpolation.c
+++ 0x1E-search_algorithms/102-interpolation.c
@@ -14,6 +14,8 @@ int interpolation_search(int *array, size_t size, int value)
 {
 	size_t low = 0, high = size - 1, pos;
 
+	if (!array)
+		return (-1);
 	while (low <= high)
 	{
 		if (array[low] == array[high])
