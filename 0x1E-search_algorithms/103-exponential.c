3862dd8b43b5b5e08106434464fedcf18a10165a
toshi-uy <s_borgia@hotmail.com>
2021-09-02 10:20:49 -0300

    added print to debug

--- 0x1E-search_algorithms/103-exponential.c
+++ 0x1E-search_algorithms/103-exponential.c
@@ -23,3 +23,40 @@ int exponential_search(int *array, size_t size, int value)
         	i = i * 2;
 	return (binary_search(array, i / 2, value));
 }
+
+/**
+ * binary_search - function that searches for a value in an array of integers
+ * using the Binary search algorithm
+ * @array: array to search in
+ * @size: size of the array
+ * @value: value to search for
+ *
+ * Return: the first index where value is located, or -1 if it fails
+ */
+
+int binary_search(int *array, size_t size, int value)
+{
+	int high, mid, low, i = 0;
+
+	if (!array)
+		return (-1);
+	low = 0;
+	high = size - 1;
+	mid = (low + high) / 2;
+
+	while (low <= high)
+	{
+		printf("Searching in array: ");
+		for (i = low; i < high; i++)
+			printf("%d, ", array[i]);
+		printf("%d\n", array[high]);
+		if (array[mid] < value)
+			low = mid + 1;
+		else if (array[mid] > value)
+			high = mid - 1;
+		else
+			return (value);
+		mid = (low + high) / 2;
+	}
+	return (-1);
+}
