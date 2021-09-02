c58c37773670e55ec519696cb1df3247ae163c01
toshi-uy <s_borgia@hotmail.com>
2021-09-02 10:42:45 -0300

    added print

--- 0x1E-search_algorithms/103-exponential.c
+++ 0x1E-search_algorithms/103-exponential.c
@@ -1,8 +1,8 @@
 #include "search_algos.h"
 
 /**
- * exponential_search - function that searches for a value in an array of integers
- * using the Exponential search algorithm
+ * exponential_search - function that searches for a value in an array
+ * of integers using the Exponential search algorithm
  * @array: array to search in
  * @size: size of the array
  * @value: value to search for
