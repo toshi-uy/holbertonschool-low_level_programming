8d0481045a851c96d901419d1c8e7139e60e9f39
toshi-uy <s_borgia@hotmail.com>
2021-09-04 19:19:59 -0300

    added file 106

--- 0x1E-search_algorithms/106-linear_skip.c
+++ 0x1E-search_algorithms/106-linear_skip.c
@@ -25,7 +25,7 @@ skiplist_t *linear_skip(skiplist_t *list, int value)
 		if (aux->express->n < value)
 		{
 			printf("Value checked at index [%lu] = [%d]\n", aux->express->next->index, aux->express->next->n);
-			aux = aux->exress->next;
+			aux = aux->express->next;
 		}
 		else
 		{
