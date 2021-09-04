82deb02b6703a44be58808215c9547bb5465caa5
toshi-uy <s_borgia@hotmail.com>
2021-09-04 17:15:23 -0300

    added jump list, and aux struct and prototypes in header

---
+++ 0x1E-search_algorithms/105-jump_list.c
@@ -0,0 +1,48 @@
+#include "search_algos.h"
+#include <math.h>
+
+/**
+ * *jump_search - function that searches for a value in an array of integers
+ * using the Jump search algorithm
+ * @array: array to search in
+ * @size: size of the array
+ * @value: value to search for
+ *
+ * Return: the first index where value is located, or -1 if it fails
+ */
+
+listint_t *jump_list(listint_t *list, size_t size, int value)
+{
+	size_t jump, j = 0, i;
+	listint_t *aux = list, *jumped;
+
+	if (!list)
+		return (NULL);
+	jump = sqrt(size);
+	if (value < list->n)
+		return (NULL);
+	while (aux && j + jump < size)
+	{
+		jumped = aux;
+		for (i = 0; i < jump && jumped; i++)
+			jumped = jumped->next;
+		if (jumped->n < value)
+		{
+			printf("Value checked array[%lu] = [%d]\n", j + jump, jumped->n);
+			j += jump;
+			aux = jumped;
+		}
+		else
+			break;
+	}
+	printf("Value found between indexes [%lu] and [%lu]\n", j, (j + jump));
+	while (j < size)
+	{
+		printf("Value checked array[%lu] = [%d]\n", j, aux->n);
+		if (aux->n == value)
+			return (j);
+		j++;
+		aux = aux->next;
+	}
+	return (NULL);
+}
--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -6,6 +6,23 @@
 #include <stdlib.h>
 #include <string.h>
 
+/**
+ * struct listint_s - singly linked list
+ *
+ * @n: Integer
+ * @index: Index of the node in the list
+ * @next: Pointer to the next node
+ *
+ * Description: singly linked list node structure
+ * for Holberton project
+ */
+typedef struct listint_s
+{
+    int n;
+    size_t index;
+    struct listint_s *next;
+} listint_t;
+
 /* Prototypes */
 int linear_search(int *array, size_t size, int value);
 int binary_search(int *array, size_t size, int value);
@@ -15,6 +32,7 @@ int jump_search(int *array, size_t size, int value);
 int interpolation_search(int *array, size_t size, int value);
 int exponential_search(int *array, size_t size, int value);
 int advanced_binary(int *array, size_t size, int value);
+listint_t *jump_list(listint_t *list, size_t size, int value);
 
 /* Auxiliary prototypes */
 int binary_search_2(int *array, size_t size, size_t pos, int value);
